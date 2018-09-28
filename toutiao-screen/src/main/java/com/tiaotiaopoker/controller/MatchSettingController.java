ac64542cb01b2f867fdb968342a68d9ed21adcd4
maomao <343669636@qq.com>
2018-09-28 21:30:40 +0800

    样式

---
+++ toutiao-screen/src/main/java/com/tiaotiaopoker/controller/MatchSettingController.java
@@ -0,0 +1,17 @@
+package com.tiaotiaopoker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("matchSetting")
+public class MatchSettingController {
+
+    @RequestMapping("index")
+    public ModelAndView index(ModelAndView mv){
+
+        mv.setViewName("matchSetting/matchIndex");
+        return  mv;
+    }
+}
