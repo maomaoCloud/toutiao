9874bf2212952accc2f9aa49a9e9d80e4aa7664d
apple <apple@192.168.30.100>
2018-10-13 18:06:25 +0800

    loading

--- toutiao-screen/src/main/resources/static/js/global.js
+++ toutiao-screen/src/main/resources/static/js/global.js
@@ -1,1 +1,16 @@
 var isStop = false;
+
+/**显示信息*/
+function showNotification(title, msg) {
+    $.jGrowl(msg, {header: title});
+}
+
+function showLoading() {
+    $("#loader").fadeIn(300);
+    $(".mask").fadeIn(300);
+}
+
+function closeLoading() {
+    $("#loader").fadeOut(300);
+    $(".mask").fadeOut(300);
+}
